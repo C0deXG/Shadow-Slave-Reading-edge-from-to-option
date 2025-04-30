3bfaba6fc9e443261ea246062d08d63db1eef6ed
COdXG <jjhjjff4@gmail.com>
2025-04-30 13:37:40 -0500

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   // Remove standalone output as it's causing issues with Vercel deployment
   webpack: (config) => {
     config.module.rules.push({
